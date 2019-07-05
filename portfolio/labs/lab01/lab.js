fb5558179a2bc4c642b55896aa249400f530b56e
well50504 <52515342+well50504@users.noreply.github.com>
2019-07-05 17:41:16 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,6 +4,47 @@
 
 
 function start(e) {
+    var name = [
+    {
+        char: '徐',
+        big5: 'AE7D',
+        cns:  '1-5541',
+        unicode: '5F90',
+
+    },
+    {
+        char: '煒',
+        big5: 'DE6D',
+        cns:  '2-4453',
+        unicode: '7152',
+
+    },
+    {
+        char: '彤',
+        big5: 'A7CD',
+        cns:  '1-4A2F',
+        unicode: '5F64',
+
+    },
+
+
+    ];
+
+    var ele = document.getElementById('div1');
+
+    for(var i = 0; i < 3; i++){
+       ele.innerHTML += name[i].char;
+       ele.innerHTML += ' big5 = ';
+       ele.innerHTML += name[i].big5;
+       ele.innerHTML += ' cns = ';
+       ele.innerHTML += name[i].cns;
+       ele.innerHTML += ' unicode = ';
+       ele.innerHTML += name[i].unicode;
+       ele.innerHTML += '<br>';
+
+
+    }
+
 
 }
 
