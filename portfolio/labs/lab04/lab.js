948739f38bc30e8ce9395ce17d3d193b436e69e7
well50504 <52515342+well50504@users.noreply.github.com>
2019-07-09 17:52:15 +0800

    Add files via upload

---
+++ portfolio/labs/lab04/lab.js
@@ -0,0 +1,33 @@
+$(function(){
+    var card = ['ace-of-club.svg', 
+                'ace-of-diamonds.svg',
+                'ace-of-hearts.svg',
+                'ace-of-spades.svg'];
+        for( var i = 0; i < 16 ; i++){
+            var r = Math.floor((Math.random()*4));
+
+            $('#d2').append('<img class="issue" src="./' + card[r] +  '">');
+        }
+
+        $('#b1').on('click',function(){
+            $('.issue').hide();
+            $('#d2').html('');
+
+            for(var i = 0; i < 16; i++){
+               $('#d2').append('<img src="./joker-card.svg">');
+            }
+
+
+
+
+        });
+
+
+
+
+
+});
+
+
+
+
